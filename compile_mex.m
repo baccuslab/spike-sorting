023f9924103bdce3f15eddc50a13b751eef351cf
Bongsoo Suh <bongsoo.suh@gmail.com>
2015-07-13 16:33:27 -0700

    commiting bash script and matlab code to compile mex codes.  installmexfiles and compile_mex.m

---
+++ compile_mex.m
@@ -0,0 +1,28 @@
+% compile_mex.m
+%
+% Called by `installmexfiles` shell script.
+% Complies c and cpp source codes in subdirectory 'mexcodes' into mex codes
+% where source codes are copied into the subdirectories from `installmexfiles` shell script.
+
+% sourse codes to compile to mex files
+%source_codes = {'AutoCorr.cpp', 'CrossCorr.cpp', 'FileHeaders.cpp', 'LoadIndexSnip.cpp'};
+source_codes = {'AutoCorr.cpp', 'CrossCorr.cpp'};
+
+for ii = 1:length(source_codes)
+    if ~exist(source_codes{ii})
+        fprintf('Error finding source code. %s.c could not be found.\n', source_codes{ii});
+    end
+
+    % mex compile
+    try
+        fprintf(' %d mexing %s\n', ii, source_codes{ii});
+        mex(source_codes{ii})
+        fprintf(' Success mexing %s\n\n', source_codes{ii});
+    catch exception
+        fprintf(' Error in mexing %s\n\n', source_codes{ii});
+        msgString = getReport(exception);
+        disp(msgString)
+    end
+end
+
+exit

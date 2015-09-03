a37652087bcbe2c56c8342581c0835f175bf6edf
Lane McIntosh <lanemcintosh@gmail.com>
2015-09-03 16:45:27 -0700

    fixed bug where certain arguments to ComputeMembership during AutoCorr were singles

--- matlab/electrical-recording/sorting/clustering/ComputeMembership.m
+++ matlab/electrical-recording/sorting/clustering/ComputeMembership.m
@@ -1,5 +1,13 @@
 function membership = ComputeMembership(x,y,polygons)
 membership = zeros(size(x));
+% polygon only takes real double vectors
+if ~isa(x, 'double')
+    x = double(x);
+end
+if ~isa(y, 'double')
+    y = double(y);
+end
+
 for i = length(polygons):-1:1			% Do it in reverse order
 	if (~isempty(polygons{i}))
 			in = polygon(x,y,polygons{i}.x,polygons{i}.y); %name of mexfile is polygon not pointsinpolygon
